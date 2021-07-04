384cdf6b4be774ec8c7c71a1384db5a806f1295f
Sukhveer-Singh <sukhdeepjoganad@gmail.com>
2021-07-04 22:01:30 +0530

    third_commit

--- django_project2/settings.py
+++ django_project2/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+import os
 STATIC_URL = '/static/'
 # to deploy to heroku
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
